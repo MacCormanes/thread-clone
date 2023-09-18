e8baf58f6ea703c4faffcd86fb80dfaef9fa4d8f
MacCormanes <janmarc132@gmail.com>
2023-09-18 22:03:51 +0800

    fixed ts error on onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -13,7 +13,7 @@ async function Page() {
 
   const userData = {
     id: user?.id,
-    objectid: userInfo?._id,
+    objectId: userInfo?._id,
     username: userInfo?.username || user?.username,
     name: userInfo?.name || user?.firstName || '',
     bio: userInfo?.bio || '',
